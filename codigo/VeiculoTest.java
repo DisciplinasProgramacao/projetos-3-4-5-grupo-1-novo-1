5fdace231179c1075e610bdc180bb1ac2465c935
Fabricio Frade <fabricio.frade@sga.pucminas.br>
2022-10-26 14:45:19 -0300

    Implementação dos testes de IPVA e seguro

--- codigo/VeiculoTest.java
+++ codigo/VeiculoTest.java
@@ -6,6 +6,9 @@ import org.junit.jupiter.api.Test;
 public class VeiculoTest {
 
     static Veiculo veiculo;
+    static Veiculo caminhao = new Caminhao("CAM-001", 180);
+    static Veiculo furgao = new Furgao("FUR-001", 180);
+    static Veiculo van = new Van("VAN-001", 180);
 
     @BeforeAll
     public static void Testinit() {
@@ -20,5 +23,45 @@ public class VeiculoTest {
     @Test
     public void name() {
        veiculo.imprimeDadosVeiculoConsole();
+    }
+
+    @Test
+    public void ipvaCarro(){
+        assertEquals(7.2, veiculo.calculaIPVA());
+    }
+
+    @Test
+    public void seguroCarro(){
+        assertEquals(309, veiculo.calculaSeguro());
+    }
+
+    @Test
+    public void ipvaCaminhao(){
+        assertEquals(1.8, caminhao.calculaIPVA());
+    }
+
+    @Test
+    public void seguroCaminhao(){
+        assertEquals(2003.6, caminhao.calculaSeguro());
+    }
+
+    @Test
+    public void ipvaFurgao(){
+        assertEquals(5.4, furgao.calculaIPVA());
+    }
+
+    @Test
+    public void seguroFurgao(){
+        assertEquals(5.4, furgao.calculaSeguro());
+    }
+
+    @Test
+    public void ipvaVan(){
+        assertEquals(5.4, van.calculaIPVA());
+    }
+
+    @Test
+    public void seguroVan(){
+        assertEquals(5.4, van.calculaSeguro());
     }
 }
