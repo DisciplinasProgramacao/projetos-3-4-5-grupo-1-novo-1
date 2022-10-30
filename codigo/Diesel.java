4dc3aaf242853110c0d0b154ccc263b601884070
JVTemponi <jvictortemponi@gmail.com>
2022-10-29 21:48:55 -0300

    Classe Diesel

---
+++ codigo/Diesel.java
@@ -0,0 +1,10 @@
+public class Diesel extends Combustivel{
+
+    private static final String DESCRICAO = "Diesel";
+    private static final double PRECO = 6.65;
+    private static final double CONSUMO = 3.5;
+
+    public Diesel(){
+        super(DESCRICAO, PRECO, CONSUMO);
+    }
+}
