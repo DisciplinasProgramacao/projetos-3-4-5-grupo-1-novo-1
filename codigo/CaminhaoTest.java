5c807c6ea0accd984c7a11a396e349a2b9e86402
JVTemponi <jvictortemponi@gmail.com>
2022-10-30 21:15:08 -0300

    Caminhão

--- codigo/CaminhaoTest.java
+++ codigo/CaminhaoTest.java
@@ -16,6 +16,16 @@ public class CaminhaoTest {
         assertEquals(1000, caminhao.calculaManutecao());
     }    
 
+    @Test
+    public void TesteValidarTanque() {
+        Caminhao caminhao = new Caminhao("CAM-13", 131313);
+        Data dt = new Data(22, 10, 2022);
+        Rota rota = new Rota(100, dt, caminhao);
+        caminhao.abastecer(3);
+        caminhao.addRota(rota);
+        assertEquals(2, caminhao.getTanque());
+    } 
+
     //Para executar o teste é necessário alterar o tanque do caminhão para 25000
     @Test
     public void TesteCalcularVistoria() {
