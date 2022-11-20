f5d85c78230b41be69d66bfca94b25c71ef8640e
JVTemponi <jvictortemponi@gmail.com>
2022-11-20 00:34:39 -0300

    Inclusão Javadoc

--- codigo/Rota.java
+++ codigo/Rota.java
@@ -6,9 +6,13 @@ public class Rota {
     private double distancia = 0;
     private Veiculo veiculoRota;
 
-    public Rota() {
-    }
-
+    /**
+     * Método construtor da classe Rota, responsável por inicializar uma instância
+     * de rota e atualizar a kilometragem total do veículo vinculado.
+     * @param distancia double com a Distância da rota
+     * @param dtProducao objeto da classe Data, informando a data em que ocorreu a rota
+     * @param veiculoParaRota objeto da classe Veiculo, informando o veículo atrelado à rota
+     */
     public Rota(double distancia, Data dtProducao, Veiculo veiculoParaRota) {
         this.idRota = sequencia++;
         this.distancia = distancia;
@@ -19,25 +23,34 @@ public class Rota {
         concatenarHash();
     }
 
+    /**
+     * Método privado utilizado para criar uma string identificadora da rota. A string 
+     * é a junção lógica das informações Data + Distância + Placa do Veículo 
+     */
     private void concatenarHash(){
         String data, placa, dist;
         int intAno;
-
         intAno = Data.diaAno(this.dtProducao.getDia(), this.dtProducao.getMes(), this.dtProducao.getAno());
         data = String.valueOf(intAno);
         placa = this.veiculoRota.getPlaca();
         dist = String.valueOf(this.getDistancia());
-        this.descHash = data.concat(dist).concat(placa).concat(data);
+        this.descHash = data.concat(dist).concat(placa);
     }
 
-    public String getDescHash() {
-        return descHash;
-    }
-
+    /**
+     * Método booleano para validar se existe uma rota entre as datas especificadas
+     * no parâmetro
+     * @param dtInicio objeto da classe Data, referente à data de início da busca
+     * @param dtFinal objeto da classe Data, referente à data de fim da busca
+     * @return Retorna true caso exista ou false caso contrário
+     */
     public boolean verificaDataPeriodo(Data dtInicio, Data dtFinal) {
         return  Data.verificaDataPeriodo(dtProducao, dtInicio,  dtFinal);
     }
 
+    /**
+     * Método para imprimir rota, com as informações referentes ao id, e data em que ocorreu a rota
+     */
     public void imprimeRota() {
         System.out.println("Rota número: " + this.idRota + " Data Produção da rota: " +
             this.dtProducao.getDiaSemana() + " " + this.dtProducao.getDia() + "/" + this.dtProducao.getMes()
@@ -48,39 +61,12 @@ public class Rota {
         return distancia;
     }
 
-    /**
-     * @return int return the idRota
-     */
     public int getIdRota() {
         return idRota;
     }
 
-    /**
-     * @param idRota the idRota to set
-     */
-    public void setIdRota(int idRota) {
-        this.idRota = idRota;
-    }
-
-    /**
-     * @return Data return the dtProducao
-     */
     public Data getdtProducao() {
         return dtProducao;
-    }
-
-    /**
-     * @param dtProducao the dtProducao to set
-     */
-    public void setdtProducao(Data dtProducao) {
-        this.dtProducao = dtProducao;
-    }
-
-    /**
-     * @param distancia the distancia to set
-     */
-    public void setDistancia(double distancia) {
-        this.distancia = distancia;
     }
 
     public Veiculo getVeiculoRota() {
@@ -98,7 +84,11 @@ public class Rota {
         return this.descHash.hashCode();
     }
 
-
+    /**
+     * Método retorna uma String com os dados de rota a serem escritos no arquivo txt
+     * @return Retorna uma String com a distânica, data e placa do ceiículo vinculados
+     * à rota
+     */
     public String escreveRotaArquivo() {
         String salvaParaArquivo = 
                 + this.distancia + ";"
