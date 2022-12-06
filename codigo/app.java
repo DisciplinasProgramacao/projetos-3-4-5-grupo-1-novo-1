14acdfca9a0365fc3ba9fae30cef3a6a4c477287
JVTemponi <jvictortemponi@gmail.com>
2022-12-05 22:39:51 -0300

    ajustes menu, chamando fábrica

--- codigo/app.java
+++ codigo/app.java
@@ -1,70 +1,44 @@
+import java.io.File;
+import java.security.InvalidParameterException;
 import java.util.Scanner;
 
 public class app {
-    static Scanner obj = new Scanner(System.in);
+    static Scanner teclado = new Scanner(System.in);
     static String arquivoVeiculo = "/media/juvito/Data/Backup_lpm/projetos-3-4-5-grupo-1-novo-1/projetos-3-4-5-grupo-1-novo-1/codigo/Veiculos_ler.txt";
     static String arquivoRota = "/media/juvito/Data/Backup_lpm/projetos-3-4-5-grupo-1-novo-1/projetos-3-4-5-grupo-1-novo-1/codigo/Rotas_ler.txt";
     //static String arquivoVeiculo = "C:/Users/Mauricio/Documents/GitHub/projetos-3-4-5-grupo-1-novo-1/codigo/Veiculos_ler.txt";
     //static String arquivoRota = "C:/Users/Mauricio/Documents/GitHub/projetos-3-4-5-grupo-1-novo-1/codigo/Rotas_ler.txt";
+    static FabricaCarro carro = new FabricaCarro();
+    static FabricaVan van = new FabricaVan();
+    static FabricaFurgao furgao = new FabricaFurgao();
+    static FabricaCaminhao caminhao = new FabricaCaminhao();
+    static ColecaoFabrica todasAsFabricas;
+    static Frota frota = new Frota();
+
 
     public static void main(String[] args) throws Exception {
     
-        Veiculo veiculoParaRota = null;
-        Frota frota = new Frota();
+        Veiculo veiculoParaRota;
         String placaVeiculo;
         Rota novaRota = null;
         double kmRota = 0;
         Data dtProducao = null;
-
+        configurarMenuReflexao();
         frota.carregarVeiculosArquivo(arquivoVeiculo);
         frota.carregarRotasArquivo(arquivoRota);
 
         String opcao = "";
         opcao = menu(opcao);
-
+        
         while (opcao != "F") {
             switch (opcao) {
                 case "1":
-                    System.out.println("Carregando lista de veiculos em: " + arquivoVeiculo);
-                    System.out.println("Carregando lista de veiculos em: " + arquivoRota);
-                    System.out.println("\nVEICULOS:\n");
-                    frota.exibirVeiculosArquivo(arquivoVeiculo);
-                    System.out.println("\nROTAS:\n");
-                    frota.exibirRotasArquivo(arquivoRota);
+                    System.out.println(listarDadosArquivos());
                     opcao = menu(opcao);
                     break;
 
                 case "2":
-                    System.out.println("\nEntre com os dados do veiculo para incluir:\nPlaca:");
-                    String placa = obj.nextLine().toUpperCase();
-                    System.out.print("\nValor de venda:");
-                    double valorVenda = obj.nextDouble();
-                    obj.nextLine();
-                    System.out.println("\nEntre com o tipo de Veículo:");
-                    System.out.println("1 - Carro\n2 - Van\n3 - Furgão\n4 - Caminhão\n\nTipo de Veículo:");
-                    char tipo = obj.nextLine().charAt(0);
-
-                    switch (tipo) {
-                        case '1':
-                            Veiculo novoCarro = new Carro(placa, valorVenda);
-                            frota.adicionarVeiculos(novoCarro);
-                            break;
-                        case '2':
-                            Veiculo novaVan = new Van(placa, valorVenda);
-                            frota.adicionarVeiculos(novaVan);
-                            break;
-                        case '3':
-                            Veiculo novoFurgao = new Furgao(placa, valorVenda);
-                            frota.adicionarVeiculos(novoFurgao);
-                            break;
-                        case '4':
-                            Veiculo novoCaminhao = new Caminhao(placa, valorVenda);
-                            frota.adicionarVeiculos(novoCaminhao);
-                            break;
-                        default:
-                            System.out.println("opção invalida!");
-                            break;
-                    }
+                    frota.adicionarVeiculos(criarVeiculo());
                     frota.salvaVeiculosFrota(arquivoVeiculo);
                     opcao = menu(opcao);
                     break;
@@ -72,23 +46,22 @@ public class app {
                 case "3":
                     System.out.println("Entre com os dados da rota para incluir:");
                     System.out.println("\nEntre com data para a rota formato DD/MM/AAAA: ");
-                    dtProducao = Data.verificaData(obj.nextLine());
-
+                    dtProducao = Data.verificaData(teclado.nextLine());
                     System.out.print("\nQuilometros da rota:");
-                    kmRota = obj.nextDouble();
-                    clearBuffer(obj);
+                    kmRota = teclado.nextDouble();
+                    clearBuffer(teclado);
                     System.out.println("Veíulos abastecidos com autonomia para a rota de:" + kmRota + "Km.");
                     if (frota.imprimeVeiculosparaRota(kmRota) > 0) {
                         System.out.println("\nEntre com a placa do veículo para a rota:\n Procurar pela placa: ");
-                        veiculoParaRota = frota.retornaVeiculoPelaPlaca(obj.nextLine().toUpperCase());
+                        veiculoParaRota = frota.retornaVeiculoPelaPlaca(teclado.nextLine().toUpperCase());
                         novaRota = new Rota(kmRota, dtProducao, veiculoParaRota);
                         if (frota.addRota(novaRota, veiculoParaRota) == 1) {
                             System.out.println("Rota adicionada com sucesso.");
-                            frota.addRota(novaRota, veiculoParaRota);
+                            //frota.addRota(novaRota, veiculoParaRota);
                         }
                         else if(frota.addRota(novaRota, veiculoParaRota) == 2){
                             System.out.println("Rota adicionada e veiculo abastecido com o combustível de maior autonomia.");
-                            frota.addRota(novaRota, veiculoParaRota);
+                            //frota.addRota(novaRota, veiculoParaRota);
                         }
                         else{System.out.println("O veiculo não suporta a distância da rota");}
                     }
@@ -98,10 +71,10 @@ public class app {
 
                 case "4":
                     System.out.println("\nEntre com a placa do Veículo para adicionar o custo extra:");
-                    placaVeiculo = obj.nextLine().toUpperCase();
+                    placaVeiculo = teclado.nextLine().toUpperCase();
                     veiculoParaRota = frota.retornaVeiculoPelaPlaca(placaVeiculo);
                     System.out.printf("\nInforme o valor do custo extra: R$");
-                    veiculoParaRota.addCustosExtra(obj.nextDouble());
+                    veiculoParaRota.addCustosExtra(teclado.nextDouble());
                     veiculoParaRota.calculaCustoVariavel();
                     System.out.println("O total de custos extras atual é: R$" + veiculoParaRota.custosExtra);
                     opcao = menu(opcao);
@@ -109,7 +82,7 @@ public class app {
 
                 case "5":
                     System.out.println("\nEntre com a placa do Veículo para localizar:");
-                    placaVeiculo = obj.nextLine().toUpperCase();
+                    placaVeiculo = teclado.nextLine().toUpperCase();
                     veiculoParaRota = frota.retornaVeiculoPelaPlaca(placaVeiculo);
                     System.out.println(veiculoParaRota);
                     opcao = menu(opcao);
@@ -117,7 +90,7 @@ public class app {
 
                 case "6":
                     System.out.println("\nEntre com a placa do Veículo para imprimir gastos:");
-                    placaVeiculo = obj.nextLine().toUpperCase();
+                    placaVeiculo = teclado.nextLine().toUpperCase();
                     if (frota.retornaVeiculoPelaPlaca(placaVeiculo) != null) {
                         veiculoParaRota = frota.retornaVeiculoPelaPlaca(placaVeiculo);
                         System.out.println(veiculoParaRota.gastosVeiculo());
@@ -144,9 +117,9 @@ public class app {
                     Data dtInicial = new Data();
                     Data dtFinal = new Data();
                     System.out.println("Entre com data inicial no formato DD/MM/AAAA: ");
-                    dtInicial = Data.verificaData(obj.nextLine());
+                    dtInicial = Data.verificaData(teclado.nextLine());
                     System.out.println("Entre com data final no formato DD/MM/AAAA: ");
-                    dtFinal = Data.verificaData(obj.nextLine());
+                    dtFinal = Data.verificaData(teclado.nextLine());
                     frota.rotasEntreDatas(dtInicial, dtFinal);
                     opcao = menu(opcao);
                     break;
@@ -162,7 +135,7 @@ public class app {
                     break;
             }
         }
-        obj.close();
+        teclado.close();
     }
 
     private static void clearBuffer(Scanner scanner) {
@@ -184,7 +157,61 @@ public class app {
         System.out.println("9  -  Veículos por custos gerados decrescente.");
         System.out.println("10 -  Filtro para busca de rotas entre datas.");
         System.out.println("\nEntre com o número correspondente à função ou 'f' para finalizar:\n");
-        opc = obj.nextLine().toUpperCase();
+        opc = teclado.nextLine().toUpperCase();
         return opc;
     }
+
+    static void configurarMenuColecao(){
+        todasAsFabricas = new ColecaoFabrica();
+        todasAsFabricas.acrescentarFabrica("1", new FabricaCarro());
+        todasAsFabricas.acrescentarFabrica("2", new FabricaVan());
+        todasAsFabricas.acrescentarFabrica("3", new FabricaFurgao());
+        todasAsFabricas.acrescentarFabrica("4", new FabricaCaminhao());
+    }
+
+    public static Veiculo criarVeiculo() throws Exception{
+        String[] veiculos = {"CARRO","VAN","FURGAO","CAMINHAO"};
+        Veiculo novoVeiculo= null;;
+        int opcao;
+        System.out.println("==========================");
+        System.out.println("INCLUIR VEICULOS");
+        for(int i = 1; i<=veiculos.length; i++){
+            System.out.println(i+" - "+veiculos[i-1]);
+        }                
+        System.out.print("Escolha: ");
+        opcao = Integer.parseInt(teclado.nextLine());
+        String tipo= veiculos[opcao-1];
+        System.out.print("Informe a placa e o valor do veiculo");
+        System.out.print("Placa: ");
+        String placa = teclado.nextLine().toUpperCase();
+        System.out.print("Valor: ");
+        double valor = teclado.nextDouble();
+        try{
+            novoVeiculo = todasAsFabricas.criar(tipo,placa,valor);
+        }catch(InvalidParameterException ipe){
+            System.out.println("Tipo de veiculo inválido.");
+        }
+        return novoVeiculo;
+    }
+    
+    static void configurarMenuReflexao() throws Exception{
+        todasAsFabricas = new ColecaoFabrica();
+        Scanner leitor = new Scanner(new File("fabricas.txt"));
+        while(leitor.hasNextLine()){
+            String[] dadosFabrica = leitor.nextLine().split(";");
+            todasAsFabricas.acrescentarFabrica(dadosFabrica[0], 
+                    (IFabrica)Class.forName(dadosFabrica[1])
+                    .getConstructor().newInstance());
+        }
+    }
+
+    static String listarDadosArquivos(){
+        StringBuilder dadosArquivos = new StringBuilder("\nCarregando lista de veiculos em: " + arquivoVeiculo);
+        dadosArquivos.append("Carregando lista de rotas em: " + arquivoRota
+                            + "\nVEICULOS:"
+                            + frota.exibirVeiculosArquivo(arquivoVeiculo)
+                            + "\nROTAS:"
+                            + frota.exibirRotasArquivo(arquivoRota));
+        return dadosArquivos.toString();
+    }    
 }
